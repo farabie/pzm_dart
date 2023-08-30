b0276f30d07da7fe4e012d7b384be01a9fcd883a
Muhammad Farabie <farabie024@gmail.com>
2023-08-30 10:17:09 +0700

    String

---
+++ bin/string.dart
@@ -0,0 +1,46 @@
+void main() {
+  String firstName = 'Muhammad'; // -> Disarankan Dart menggunakan petik satu
+  String lastName = "Farabie";
+
+  print(firstName);
+  print(lastName);
+
+  //String interpolation atau dengan tanda $namaVariable atau ${namaVariable}
+
+  String fullName = '$firstName ${lastName}';
+  print(fullName);
+
+  /**
+   * backslash(\) digunakan jika ada karakter seperti ''(petik satuu) atau ""
+   * (petik dua) atau $(expression)
+   */
+
+  String text = 'this is \'dart\' \$cool';
+  print(text);
+
+  /**
+   * Mengabunggkan string bisa juka menggunakan spasi, enter atau tab
+   * Jika dia tidak dalam variable maka bisa menggunakan spasi, enter atau tab
+   * Jika dalam table maka tidak bisa
+   */
+
+  // String fullName2 = firstName  lastName; -> ini tidak bisa dan error karena hanya yang tidak ada variable saja
+  String name = 'Muhammad ' 'Farabie';
+  print(name);
+
+  //Menggabungkan String
+  String fullName2 = firstName + ' ' + lastName;
+  print(fullName2);
+
+  /**
+   * Jika terdapat string yang panjang bisa menggunakan ''''''(tanda petik 3)
+   */
+
+  String text2 = '''
+ini merupakan text yang sangat panjang
+karena terlalu panjang
+makanya dibuat petik 3 hehehe bukan petik satu
+''';
+
+  print(text2);
+}
