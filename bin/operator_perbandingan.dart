5adf351cf8d785c2af64525407312afb60e368c4
Muhammad Farabie <farabie024@gmail.com>
2023-08-30 11:01:43 +0700

    operator_perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Sama dengan
+  print(10 == 10);
+  //Tidak Sama dengan
+  print(10 != 10);
+  //Lebih dari
+  print(10 > 10);
+  //Kurang dari
+  print(10 == 10);
+  //Lebih dari sama dengan
+  print(10 >= 10);
+  //Kurang dari sama dengan
+  print(10 == 10);
+
+  //Atau kita bisa bandingkan dengan string
+  print('abie' == 'abie');
+  print('abie' != 'abie');
+  // print('abie' > 'abie'); // Kalau lebih dari tidak bisa
+}
