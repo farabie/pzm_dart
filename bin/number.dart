7c867019313c5318d09802b85613e1da96f7d6a3
Muhammad Farabie <farabie024@gmail.com>
2023-08-30 09:45:31 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,15 @@
+void main() {
+  //Di dart ada dua type data number yaitu int dan double
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+
+  //Type data num dapat memuat int dan double
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+}
