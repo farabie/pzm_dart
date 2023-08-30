51e7910ea5c96d8a3ef4afaf10ae660fb0d86a4c
Muhammad Farabie <farabie024@gmail.com>
2023-08-30 09:39:34 +0700

    comment.dart

---
+++ bin/comment.dart
@@ -0,0 +1,12 @@
+///This is comment main function for dart
+///will be execute in dart doc
+void main() {
+  //this single line commen
+  var name = 'Muhammad Farabie';
+
+  /**
+   * this is multiple line
+   * this not execute in dart
+   */
+  print(name);
+}
