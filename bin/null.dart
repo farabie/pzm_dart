b612bc023d8dc5ea4b47357f93252b3637e4fcec
Muhammad Farabie <farabie024@gmail.com>
2023-08-31 08:28:11 +0700

    null

---
+++ bin/null.dart
@@ -0,0 +1,8 @@
+void main() {
+  int number1;
+  int?
+      number2; //Jika mau type data nya nullable cukup dengan typeData? namaVariable
+
+  // print(number1); -> Error karena tidak nullable
+  print(number2);
+}
