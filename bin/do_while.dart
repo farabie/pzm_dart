51020e655e825a83658b40071b2586b196409fce
Muhammad Farabie <farabie024@gmail.com>
2023-08-31 10:25:25 +0700

    do_while

---
+++ bin/do_while.dart
@@ -0,0 +1,9 @@
+void main() {
+  var counter = 100;
+
+  //Bedanya dengan while dia eksekusi dulu baru dicek kondisinya
+  do {
+    print('Perulangan Ke $counter');
+    counter++;
+  } while (counter <= 10);
+}
