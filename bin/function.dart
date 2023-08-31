4469b402d7deee77cf302c7bda6496828215b714
Muhammad Farabie <farabie024@gmail.com>
2023-08-31 10:55:19 +0700

    function

---
+++ bin/function.dart
@@ -0,0 +1,14 @@
+void sayHello() {
+  print('Hello World 1');
+  print('Hello World 2');
+}
+
+void main() {
+  /**
+   * Untuk membuat function itu mudah dengan cara void namaFunctionnya
+   * Sebenarnya print() itu juga function untuk menampilkan tulisan ke console
+   */
+
+  sayHello();
+  sayHello(); //Ini untuk memanggil function
+}
