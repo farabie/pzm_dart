c3e73bc7ce9a641a9480fc73fcea95944b74b4a0
Muhammad Farabie <farabie024@gmail.com>
2023-09-01 08:36:18 +0700

    function_parameter

---
+++ bin/function_parameter.dart
@@ -0,0 +1,8 @@
+void sayHello(String firstName, String LastName, int age) {
+  print('Halo $firstName $LastName Umur saya $age');
+}
+
+void main() {
+  sayHello('Muhammad', 'Farabie', 19);
+  sayHello('Abie', 'Nurmala', 20);
+}
